a7122bff7ac6def0c86ccee20b5986b888bf2c00
insh007 <aliinshad007@gmail.com>
2023-05-06 00:08:40 +0530

    connected client-server successfully

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 require('dotenv').config()
 const express = require('express')
 const route = require('./src/routes/routes')
+const cors = require('cors')
 
 const app = express()
 app.use(express.json())
 
+app.use(cors({
+    origin: ["http://localhost:5173"]
+}))
+
+// Add the middleware function here
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use('/', route)
 
-app.use("/*", function(req,res){
+app.use("/*", function (req, res) {
     res.status(400).send("Provided url is wrong")
 })
 
-app.listen(process.env.PORT, () => console.log("App is running on Port",process.env.PORT))
+app.listen(process.env.PORT, () => console.log("App is running on Port", process.env.PORT))
--- src/controller/priceAPI.js
+++ src/controller/priceAPI.js
@@ -1,8 +1,17 @@
+/*
+Note : we can use axios package to fetch data to mimic the required output but after searching I did not find any
+       free API which mimic the required output so I randomly generate the required output to match with the output.
+
+       Additonally I make the frontend which is mentioned in the assignment by choice and also for implementing 
+       the Authentication part & to save user data in Database I used MongoDB so indirectly this project converts 
+       into a MERN stack project.
+*/
+
 const fetchData = async function (req, res) {
     try {
-        const { Source, Destination, Date } = req.body;
+        const { Source, Destination, Date } = req.query;
 
-        // Perform any necessary input validation
+        // Perform necessary input validation
         if (!Source || !Destination || !Date) {
             return res.status(400).send({ status: false, error: 'Missing required parameters' });
         }
@@ -11,9 +20,9 @@ const fetchData = async function (req, res) {
         const prices = {};
 
         airlines.forEach((airline) => {
-            const minPrice = 1000; // Minimum price
-            const maxPrice = 5000; // Maximum price
-            const randomPrice = Math.floor(Math.random() * (maxPrice - minPrice)) + minPrice;
+            const minPrice = 1000; // Assuming Minimum price
+            const maxPrice = 5000; // Assuming Maximum price
+            const randomPrice = Source==Destination ? 0 : Math.floor(Math.random() * (maxPrice - minPrice))+ minPrice;
             prices[airline] = `₹${randomPrice}`;
         });
 
